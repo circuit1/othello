a4f3cb32fb4b179b65766f5ec2ee05833961aeb6
CS24 <tswang@caltech.edu>
2018-03-02 11:25:46 -0800

    Beat SimplePlayer

--- player.cpp
+++ player.cpp
@@ -32,38 +32,42 @@ Player::Player(Side side) {
 Player::~Player() {
 }
 
-/*
-* Heuristic function takes a board position and returns a numeric "score"
-* TODO: We need to be able to call this function in doMove() and have
-*       it pick the best possible move somehow
-*/
+/**
+ * Heuristic function takes a board position and returns a numeric "score"
+ *
+ */
 int Player::Heuristic(Move *my_move){
     Side opponent = WHITE;
-    int position_score, moves_score, corner_score;
+    int position_score, corner_score, moves_score;
     if (s == WHITE) {
         opponent = BLACK;
     }
     // score for # of stones on board
-    position_score = board->count(s) - board->count(opponent) + 1;
+    position_score = board->count(s) - board->count(opponent);
 
-    // Score for # of possible moves -> the more moves, the better
-    if (board->hasMoves(s) || board->hasMoves(opponent)){
+
+    // Score for # of possible moves after 
+    // we make a move -> the more moves, the better
+    Board *test_board = board->copy();
+    test_board->doMove(my_move, s);
+    if (test_board->hasMoves(s)){
         int num_op_moves = 0, my_moves = 0;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 Move move(i, j);
-                if (board->checkMove(&move, s)) my_moves++;
-                if (board->checkMove(&move, opponent)) num_op_moves++;
+                if (test_board->checkMove(&move, s)) my_moves++;
+                if (test_board->checkMove(&move, opponent)) num_op_moves++;
             }
         }
-        moves_score = my_moves - num_op_moves;
+        moves_score = my_moves;
     }
     else {
-        moves_score = 0;
+        moves_score = -50;
     }
 
+
     // Score for moves adjacent to corners
-    // -5 score for (1,1), (6,1), (1,6), (6, 6) diagnoal to corners
+    // -10 score for (1,1), (6,1), (1,6), (6, 6) diagonal to corners
     int my_corners = 0, op_corners = 0;
     if (board->get(s, 1, 1)) my_corners -= 5;
     if (board->get(opponent, 1, 1)) op_corners -= 5;
@@ -82,24 +86,36 @@ int Player::Heuristic(Move *my_move){
     if (X == 1 && Y == 6) my_corners -= 5;
     if (X == 6 && Y == 6) my_corners -= 5;
 
-    // -3 score for (0,1), (1,0), (0,6), (6,0), (7,1), (1,7), (7, 6), (6,7)
+    // -5 score for (0,1), (1,0), (0,6), (6,0), (7,1), (1,7), (7, 6), (6,7)
     // which are adjacent to corners
-    if (board->get(s, 0, 1)) my_corners -= 3;
-    if (board->get(opponent, 0, 1)) op_corners -= 3;
-    if (board->get(s, 0, 6)) my_corners -= 3;
-    if (board->get(opponent, 0, 6)) op_corners -= 3;
-    if (board->get(s, 1, 0)) my_corners -= 3;
-    if (board->get(opponent, 1, 0)) op_corners -= 3;
-    if (board->get(s, 1, 7)) my_corners -= 3;
-    if (board->get(opponent, 1, 7)) op_corners -= 3;
-    if (board->get(s, 6, 0)) my_corners -= 3;
-    if (board->get(opponent, 6, 0)) op_corners -= 3;
-    if (board->get(s, 6, 7)) my_corners -= 3;
-    if (board->get(opponent, 6, 7)) op_corners -= 3;
-    if (board->get(s, 7, 1)) my_corners -= 3;
-    if (board->get(opponent, 7, 1)) op_corners -= 3;
-    if (board->get(s, 7, 6)) my_corners -= 3;
-    if (board->get(opponent, 7, 6)) op_corners -= 3;
+    if (board->get(s, 0, 1)) my_corners -= 5;
+    if (board->get(opponent, 0, 1)) op_corners -= 5;
+    if (board->get(s, 0, 6)) my_corners -= 5;
+    if (board->get(opponent, 0, 6)) op_corners -= 5;
+    if (board->get(s, 1, 0)) my_corners -= 5;
+    if (board->get(opponent, 1, 0)) op_corners -= 5;
+    if (board->get(s, 1, 7)) my_corners -= 5;
+    if (board->get(opponent, 1, 7)) op_corners -= 5;
+    if (board->get(s, 6, 0)) my_corners -= 5;
+    if (board->get(opponent, 6, 0)) op_corners -= 5;
+    if (board->get(s, 6, 7)) my_corners -= 5;
+    if (board->get(opponent, 6, 7)) op_corners -= 5;
+    if (board->get(s, 7, 1)) my_corners -= 5;
+    if (board->get(opponent, 7, 1)) op_corners -= 5;
+    if (board->get(s, 7, 6)) my_corners -= 5;
+    if (board->get(opponent, 7, 6)) op_corners -= 5;
+
+
+    // Check (0,1), (1,0), (0,6), (6,0), (7,1), (1,7), (7, 6), (6,7)
+    // for our input
+    if (X == 0 && Y == 1) my_corners -= 5;
+    if (X == 1 && Y == 0) my_corners -= 5;
+    if (X == 0 && Y == 6) my_corners -= 5;
+    if (X == 6 && Y == 0) my_corners -= 5;
+    if (X == 7 && Y == 1) my_corners -= 5;
+    if (X == 1 && Y == 7) my_corners -= 5;
+    if (X == 7 && Y == 6) my_corners -= 5;
+    if (X == 6 && Y == 7) my_corners -= 5;
 
 
     // Check (0,1), (1,0), (0,6), (6,0), (7,1), (1,7), (7, 6), (6,7)
@@ -114,25 +130,25 @@ int Player::Heuristic(Move *my_move){
     if (X == 6 && Y == 7) my_corners -= 3;
 
     // +7 score for moves in corners
-    if (board->get(s, 0, 0)) my_corners += 7;
-    if (board->get(opponent, 0, 0)) op_corners += 7;
-    if (board->get(s, 0, 7)) my_corners += 7;
-    if (board->get(opponent, 0, 7)) op_corners += 7;
-    if (board->get(s, 7, 0)) my_corners += 7;
-    if (board->get(opponent, 7, 0)) op_corners += 7;
-    if (board->get(s, 7, 7)) my_corners += 7;
-    if (board->get(opponent, 7, 7)) op_corners += 7;
+    if (board->get(s, 0, 0)) my_corners += 10;
+    if (board->get(opponent, 0, 0)) op_corners += 10;
+    if (board->get(s, 0, 7)) my_corners += 10;
+    if (board->get(opponent, 0, 7)) op_corners += 10;
+    if (board->get(s, 7, 0)) my_corners += 10;
+    if (board->get(opponent, 7, 0)) op_corners += 10;
+    if (board->get(s, 7, 7)) my_corners += 10;
+    if (board->get(opponent, 7, 7)) op_corners += 10;
+
 
 
     // Check our input for corner moves
-    if (X == 0 && Y == 0) my_corners += 7;
-    if (X == 0 && Y == 7) my_corners += 7;
-    if (X == 7 && Y == 0) my_corners += 7;
-    if (X == 7 && Y == 7) my_corners += 7;
+    if (X == 0 && Y == 0) my_corners += 10;
+    if (X == 0 && Y == 7) my_corners += 10;
+    if (X == 7 && Y == 0) my_corners += 10;
+    if (X == 7 && Y == 7) my_corners += 10;
 
     corner_score = my_corners - op_corners;
-
-    return position_score + moves_score + corner_score;
+    return position_score + 10 * corner_score + 20 * moves_score;
     
 }
 
@@ -152,15 +168,16 @@ int Player::Heuristic(Move *my_move){
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
 
     // Pick a valid move based on the heuristic function
-    int max_score = 0;
-    Move move(0, 0);
-    int x = 0;
-    int y = 0;
     // Iterate through all possible moves and choose
     // the move with the max heuristic
     if (board->hasMoves(s))
     {
-       for (int i = 0; i < 8; i++) {
+        int max_score = 0;
+        Move move(0, 0);
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 Move test_move(i, j);
                 if (board->checkMove(&move, s)) {
@@ -173,6 +190,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
                 } 
             }
         }
+
         return new Move(x, y);
     }
 
